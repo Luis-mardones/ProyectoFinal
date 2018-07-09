194a03ec12e8271760f65640bc364f96eef67551
Luis-mardones <luis.mardones2017@twk.cl>
2018-07-09 11:12:10 -0400

    se agrego los import de angular material a utilizar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,6 @@
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -10,6 +13,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    MatButtonModule,
+    MatInputModule,
+    BrowserAnimationsModule,
     BrowserModule,
     FormsModule,
     HttpModule
